a9dc10be585e27885edaa209c96007c91be66613
Jennifer Mikac <jennymikac@gmail.com>
2020-06-30 13:40:37 -0400

    Added h2
    Added h2 to indicate that user can reload for a fresh roll

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+
 document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
 
 //Declare the winner
-
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").textContent = "🏴‍ Player 1 Wins!";
 } else if (randomNumber1 === randomNumber2) {
